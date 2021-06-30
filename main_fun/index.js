7bb5deafcfc088588587faf532fe9a3f91cd99f2
dlamon1 <devon.lamond@gmail.com>
2021-06-30 01:48:47 -0700

    asdf

--- main_fun/index.js
+++ main_fun/index.js
@@ -1,32 +1,22 @@
 var ffmetadata = require("ffmetadata");
 var pathToFfmpeg = require('ffmpeg-static');
 
-
 function edit(path) {
+
+  ffmetadata.setFfmpegPath(pathToFfmpeg)
 
   let pathArr = path.split('/')
   let prefix = pathArr[pathArr.length]
   console.log(pathArr)
   console.log(pathArr.length)
 
-  // Set path to ffmpeg - optional if in $PATH or $FFMPEG_PATH
-  ffmetadata.setFfmpegPath(pathToFfmpeg);
-
-  // Read song.mp3 metadata
-  ffmetadata.read(path, function (err, data) {
-    if (err) console.error("Error reading metadata", err);
-    else console.log(data);
+  let options = {
+    title: 'test title',
+  };
+  ffmetadata.write(path, options, function (err) {
+    if (err) console.error("Error writing cover art");
+    else console.log("Cover art added");
   });
-
-  // Set the artist for song.mp3
-  // var data = {
-  //   title: prefix,
-  // };
-
-  // ffmetadata.write("song.mp3", data, function (err) {
-  //   if (err) console.error("Error writing metadata", err);
-  //   else console.log("Data written");
-  // });
 
 }
 
