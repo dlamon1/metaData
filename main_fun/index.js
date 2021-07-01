bba4c00e659f5726ef10d3072ddddbcfb4198bc1
dlamon1 <devon.lamond@gmail.com>
2021-07-01 00:18:08 -0700

    good?

--- main_fun/index.js
+++ main_fun/index.js
@@ -1,29 +1,50 @@
 const ffmpeg = require("ffmpeg-cli");
+const fs = require('fs')
+const fsPromises = fs.promises;
 
-// const options = {
-//   include: ['TALB', 'TIT2'],    // only read the specified tags (default: all)
-//   exclude: ['APIC'],            // don't read the specified tags (default: [])
-//   onlyRaw: false,               // only return raw object (default: false)
-//   noRaw: false                  // don't generate raw object (default: false)
-// }
+async function folder(arr, folderCheck) {
 
-function edit(path) {
+  return fsPromises.access(folderCheck, function (error) {
+    if (error) {
+      fs.mkdir(folderCheck, (err) => {
+        if (err) {
+          console.error(err);
+        }
+        console.log('Directory created successfully!');
+        return
+      });
+    } else {
+      console.log("Directory exists.")
+      return
+    }
+  })
+}
+
+async function edit(path) {
 
   let arr = (path.split('/'))
   let newPath = ''
+  let folderCheck = ''
+  let titleArr = arr[arr.length - 1].split('.')
+  let title = titleArr[0]
 
   for (let i = 1; i < arr.length; i++) {
     if (i != arr.length - 1) {
       newPath = newPath.concat(`/${arr[i]}`)
     } else {
-      newPath = newPath.concat('/_')
-      newPath = newPath.concat(arr[i])
+      folderCheck = newPath.concat('/UpdatedMeta/')
+      newPath = newPath.concat(`/UpdatedMeta/${arr[i]}`)
     }
   }
 
-  const cmd = `-i ${path} -metadata title="Movie Title" -metadata year="2010" -codec copy ${newPath}`
 
-  ffmpeg.run(cmd).then(result => console.log(result))
+  const cmd = `-i ${path} -metadata title=${title} -codec copy ${newPath}`
+  // const cmd = `-i ${path} -metadata title=${data.title} -metadata year="${data.year} -codec copy ${newPath}`
+
+
+  folder(arr, folderCheck).then(ffmpeg.run(cmd))
+
+  // ffmpeg.run(cmd)
 }
 
 
